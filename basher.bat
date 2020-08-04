061217034124da1a2b73380aced0aa89da0ab03d
OnionKiller <33357968+OnionKiller@users.noreply.github.com>
2020-08-04 15:28:19 +0200

    Update basher.bat
    typo fix

--- basher.bat
+++ basher.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-rem ed 'D:\Program Files\MATLAB\R2020a\bin' - alreadi part of PATH
+rem ed 'D:\Program Files\MATLAB\R2020a\bin' - already part of PATH
 copy ..\Pretty_visuals.mlx .\PrettyVisuals.mlx
 matlab -batch "matlab.internal.liveeditor.openAndConvert('PrettyVisuals.mlx','PrettyPrinted.m');exit"
