68c748578293a24257942d8f7f15c46534d17257
OnionKiller <33357968+OnionKiller@users.noreply.github.com>
2020-12-17 17:23:37 +0100

    move files automaticly to git-readable

--- basher.bat
+++ basher.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 rem ed 'D:\Program Files\MATLAB\R2020a\bin' - already part of PATH
-FOR %%I in (.\interactive\*) DO matlab -batch "matlab.internal.liveeditor.openAndConvert('%%I','%%~nI.m');exit"
+FOR %%I in (.\interactive\*) DO matlab -batch "matlab.internal.liveeditor.openAndConvert('%%I','%%~nI.m');move %%~nI.m git-readable; exit"
